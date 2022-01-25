e01537891573974cd2fa56bc9cae2aadd7834c98
sabrinapopescu <97558347+sabrinapopescu@users.noreply.github.com>
2022-01-25 01:46:56 +0100

    add mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-console.log ("Welcome to my porftolio! Check out my projects or say hey")
+var body = $("body")
+var menu = $(".menu")
+var mobileNavigation = $(".mobile-navigation")
+var isMenuOpen = false
+
+menu.on('click', function() {
+  isMenuOpen = !isMenuOpen
+
+  body.toggleClass("is-menu-open", isMenuOpen)
+  menu.toggleClass("is-open", isMenuOpen)
+  mobileNavigation.toggleClass("is-open", isMenuOpen)
+})
