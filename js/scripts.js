7d67bf29a9cb044ad72f9b7dc3a05b1f8a2af695
sabrinapopescu <97558347+sabrinapopescu@users.noreply.github.com>
2022-01-25 01:52:29 +0100

    close mobile navigation when not on mobile

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,19 @@ var isMenuOpen = false
 menu.on('click', function() {
   isMenuOpen = !isMenuOpen
 
+  toggleMenuOpen()
+})
+
+function toggleMenuOpen() {
   body.toggleClass("is-menu-open", isMenuOpen)
   menu.toggleClass("is-open", isMenuOpen)
   mobileNavigation.toggleClass("is-open", isMenuOpen)
+}
+
+$(window).on('resize', function () {
+  var width = $(window).width()
+  if (isMenuOpen && width > 768) {
+    isMenuOpen = false
+    toggleMenuOpen()
+  }
 })
