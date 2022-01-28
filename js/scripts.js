afe02fac3caeeda8c78fc8ef36fa36220d4d5b9b
sabrinapopescu <97558347+sabrinapopescu@users.noreply.github.com>
2022-01-28 21:02:19 +0100

    lint js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
-var body = $("body")
-var menu = $(".menu")
-var mobileNavigation = $(".mobile-navigation")
+var $ = window.$
+
+var body = $('body')
+var menu = $('.menu')
+var mobileNavigation = $('.mobile-navigation')
 var isMenuOpen = false
 
-menu.on('click', function() {
+menu.on('click', function () {
   isMenuOpen = !isMenuOpen
 
   toggleMenuOpen()
 })
 
-function toggleMenuOpen() {
-  body.toggleClass("is-menu-open", isMenuOpen)
-  menu.toggleClass("is-open", isMenuOpen)
-  mobileNavigation.toggleClass("is-open", isMenuOpen)
+function toggleMenuOpen () {
+  body.toggleClass('is-menu-open', isMenuOpen)
+  menu.toggleClass('is-open', isMenuOpen)
+  mobileNavigation.toggleClass('is-open', isMenuOpen)
 }
 
 $(window).on('resize', function () {
